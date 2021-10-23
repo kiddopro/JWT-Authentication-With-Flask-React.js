a19247553662eac3f5744f08e82067630c06e0b8
Martín Suárez <ms.prgramador@gmail.com>
2021-10-23 04:24:10 +0000

    api login - signup created

--- src/api/routes.py
+++ src/api/routes.py
@@ -12,30 +12,25 @@ from flask_jwt_extended import get_jwt_identity
 api = Blueprint('api', __name__)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
-
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend"
-    }
-
-    return jsonify(response_body), 200
-
-# login de prueba
+# login
 @api.route("/login", methods=["POST"])
 def login():
     email = request.json.get("email", None)
     password = request.json.get("password", None)
 
+    # filtramos los usuarios segun su email y contraseña
     user = User.query.filter_by(email=email, password=password).first()
 
+    # si no encuentra match en la base de datos retorna el mensaje correspondiente
     if user is None:
         return jsonify({"msg": "Bad email or password"}), 401
     else:
-        access_token = create_access_token(identity=email)
-        return jsonify(access_token=access_token)
+        # de lo contrario se crea un token haciendo referencia a la id del usuario
+        access_token = create_access_token(identity=user.id)
+        # retornamos un json con los datos del token y la id de usuario a la que se le esta asignando el mismo
+        return jsonify({"token": access_token, "user_id": user.id })
 
-
+# signup
 @api.route("/signup", methods=['POST'])
 def signup():
     request_body = request.json
@@ -44,3 +39,6 @@ def signup():
     db.session.add(user)
     db.session.commit()
     return jsonify({"msg": "User created"}), 200
+
+# private
+# @api.route('/private', methods=['GET'])
