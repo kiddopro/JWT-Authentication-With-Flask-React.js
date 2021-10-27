0483b5ae02acab21c99dd61043c7dd077e6826ce
Martín Suárez <ms.prgramador@gmail.com>
2021-10-27 19:11:14 +0000

    models added

--- src/api/models.py
+++ src/api/models.py
@@ -12,7 +12,7 @@ class User(db.Model):
     favorites_planet = db.relationship('FavoritesPlanets', backref="user")
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.email
 
     def serialize(self):
         return {
